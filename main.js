0e18fdf6ec0ef77dec8faed99c73e55a43230ef1
Yan Foto <yan.foto@quaintous.com>
2015-08-08 21:24:52 +0200

    Add main file

---
+++ main.js
@@ -0,0 +1,33 @@
+var app = require('app');
+var chokidar = require('chokidar');
+
+module.exports = function(glob, options) {
+  var browserWindows = [];
+  var opts = options || {ignored: /[\/\\]\./};
+
+  var onChange = function() {
+    browserWindows.forEach(function(bw) {
+      bw.webContents.reloadIgnoringCache();
+    });
+  };
+
+  var watcher = chokidar.watch(glob, opts);
+
+  // Due to this issue: https://github.com/atom/electron/issues/2455
+  // I suppose this is our best chance to know when a BW is created
+  app.on('browser-window-focus', function(e, bw) {
+    var i = browserWindows.indexOf(bw);
+
+    // A new browser window
+    if(i === -1) {
+      browserWindows.push(bw);
+
+      // Remove reference when BrowserWindow is closed
+      bw.on('closed', function() {
+        browserWindows.splice(i, 1);
+      });
+    }
+  });
+
+  watcher.on('change', onChange);
+};
